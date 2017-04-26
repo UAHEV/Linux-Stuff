c01668f2c218812e706b1e6e93d5f8a4a48ac7fc
willBoyd8 <will.boyd@abwlabs.com>
2017-04-26 12:53:56 -0500

    Added the I2C python scripts

---
+++ i2cScripts/requestI2C.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+### This code does not yet function in full form ###
+
+import argparse
+import subprocess
+from smbus2 import SMBusWrapper
+
+
+### Set up the command line stuff
+parser = argparse.ArgumentParser(description='Request integer from address')
+parser.add_argument('bus', metavar='b', help='bus to request from (0 or 1)')
+parser.add_argument('address', metavar='a', type=str, help='address to request from (in hex, ex: 0x04)')
+#parser.add_argument('number', metavar='n', type=str, help='number of bytes to request')
+
+### parse arguments
+args = parser.parse_args()
+
+### send the data
+with SMBusWrapper(args.bus) as bus:	
+	### Read a block of 16 bytes from address 80, offset 0
+    	block = bus.read_i2c_block_data(0x08, 0, 6)
+    	### Returned value is a list of 16 bytes
+    	print(block)
+#	try:
+#    	bus.write_i2c_block_data(args.address, 0, bytearray(args.data))
+#	except IOError:
+#   		subprocess.call(['i2cdetect', '-y', '1'])
+#  		flag = 1
