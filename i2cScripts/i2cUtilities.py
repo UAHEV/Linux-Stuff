9b74374869639f09317b60ec73840ccd901fae14
willBoyd8 <willBoyd8@users.noreply.github.com>
2017-05-09 23:00:39 -0500

    Fixed SMBusWrapper

--- i2cScripts/i2cUtilities.py
+++ i2cScripts/i2cUtilities.py
@@ -16,6 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from smbus2 import SMBusWrapper
+
 ######################################################################################
 # transmitI2C - Utility for transmitting data over I2C.                              #
 #                                                                                    #
